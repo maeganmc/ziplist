fdcc5663331753c8cee7a7d4150534d740e5c635
maeganmc <maeganmc@users.noreply.github.com>
2021-02-09 03:01:15 -0500

    commit

---
+++ ziplist.js
@@ -0,0 +1,11 @@
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
+
+function zipList(list1, list2) {
+  const resultList = [];
+  for (let i = 0; i < list1.length; i++) {
+    resultList.push(list1[i], list2[i]);
+  }
+  return resultList;
+}
+
